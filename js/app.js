f877e72e9e487645116e91819f10af063981c893
mijan-joy <mmrsm46@gmail.com>
2022-02-17 18:28:43 +0600

    function updatedd

--- js/app.js
+++ js/app.js
@@ -8,30 +8,23 @@ calculateBtn.addEventListener('click', function(){
   const itemName3 = document.getElementById('item-input-3');
  
   const total = parseInt(itemName1.value) + parseInt(itemName2.value) + parseInt(itemName3.value);
-  
-  const totalToDisplay = document.getElementById('sub-total');
+  const totalToDisplay = document.getElementById('expenses-total');
   totalToDisplay.innerText = total;
-  
   const balance = document.getElementById('balance');
   balance.innerText = parseInt(income.value) - total;
-
- 
-
 });
 
 // Balance  percet
-
-
 const saveBtn = document.getElementById('saving');
 
 saveBtn.addEventListener('click', function(){
   const income = document.getElementById('income-input');  
   const percent = document.getElementById('percent-input');
-  
   const savings = parseInt(income.value) / parseInt(percent.value);
   const savingDisplay = document.getElementById('saving-amount');
   savingDisplay.innerText = savings;
-  // console.log(savings);
-})
+  // remainingBalance
+  const remainingBalance = document.getElementById('remaining-balance');
 
-// Remaining Balance
+  remainingBalance.innerText = parseInt(balance.innerText) - parseInt(savingDisplay.innerText);
+})
